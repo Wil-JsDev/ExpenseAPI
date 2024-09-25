7b9e0eea845cfe5b97c7f1ad25c1b1e830a8c36f
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-09-24 22:36:12 -0400

    feat: add models and settings

---
+++ ExpenseAPI.Domain/Entities/Expense.cs
@@ -0,0 +1,28 @@
+﻿using ExpenseAPI.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseAPI.Domain.Entities
+{
+    public class Expense
+    {
+        public int ExpenseId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime ExpenseDate { get; set; }
+
+        // Foreign Key to Category
+        public int CategoryId { get; set; }  // Foreign Key
+
+        public Category Category { get; set; }  // Navigation property
+
+        public string Description { get; set; }
+
+        public DateTime Created_at { get; set; } = DateTime.UtcNow;
+
+    }
+}
