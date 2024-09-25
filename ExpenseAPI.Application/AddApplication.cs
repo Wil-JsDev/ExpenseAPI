398446ba703284cbb2da475f66a7f7258b0d4cb9
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-09-24 22:46:44 -0400

    feat: add identity models

---
+++ ExpenseAPI.Application/AddApplication.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseAPI.Application.Interfaces.Service;
+using ExpenseAPI.Application.Mapping;
+using ExpenseAPI.Application.Service;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseAPI.Application
+{
+    public static class AddApplication
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            #region Mapper
+            services.AddAutoMapper(typeof(MappingProfile));
+            #endregion
+
+            #region Services
+            services.AddScoped<IExpenseService, ExpenseService>();
+
+            #endregion
+        }
+    }
+}
